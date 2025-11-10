e88137300f5228ab3146a3f6ad8b5b38196ca01b
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-11-10 19:37:34 +0200

    Pylint toivottavasti toimii

--- src/index.py
+++ src/index.py
@@ -1,26 +1,15 @@
 from varasto import Varasto
 
-
 def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
-    print(f"Olut: saldo={olutta.saldo}, tilavuus={olutta.tilavuus}")
-
+    mehua, olutta = Varasto(100.0), Varasto(100.0, 20.2)
+    print(f"Luonnin jälkeen:\nMehuvarasto: {mehua}\nOlutvarasto: {olutta}\nOlut: saldo={olutta.saldo}, tilavuus={olutta.tilavuus}")
     mehua.lisaa_varastoon(50.7)
-    print("Lisätään 50.7")
-    print(f"Mehuvarasto: {mehua}")
-
+    print(f"Lisätään 50.7\nMehuvarasto: {mehua}")
     print("Virhetilanteita:")
-    virhe = Varasto(-100.0)
-    print(virhe)
-
+    print(Varasto(-100.0))
     olutta.lisaa_varastoon(1000.0)
-    print("Olut täytetty yli")
-    print(f"Olutvarasto: {olutta}")
+    print(f"Olut täytetty yli\nOlutvarasto: {olutta}")
 
 
 if __name__ == "__main__":
