de9348c702d5a43e2fa0caf1e18b2e4eefd1f159
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-11-10 19:42:33 +0200

    nyt ehkä menee Pylint läpi

--- src/index.py
+++ src/index.py
@@ -3,7 +3,10 @@ from varasto import Varasto
 def main():
 
     mehua, olutta = Varasto(100.0), Varasto(100.0, 20.2)
-    print(f"Luonnin jälkeen:\nMehuvarasto: {mehua}\nOlutvarasto: {olutta}\nOlut: saldo={olutta.saldo}, tilavuus={olutta.tilavuus}")
+    print(f"Luonnin jälkeen:\nMehuvarasto: {mehua}\n"
+          f"Olutvarasto: {olutta}\n"
+          f"Olut: saldo = {olutta.saldo}, tilavuus = {olutta.tilavuus}"
+        )
     mehua.lisaa_varastoon(50.7)
     print(f"Lisätään 50.7\nMehuvarasto: {mehua}")
     print("Virhetilanteita:")
